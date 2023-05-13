e252ad9a146a10ac64ef9d0f31a0d628bea8838d
mindytyj <derpbritz@gmail.com>
2023-05-13 23:04:22 +0800

    Ver 3: Define required constants

---
+++ main.js
@@ -0,0 +1,33 @@
+/*----- constants -----*/
+const colors = {
+  player1: "white",
+  player2: "yellow",
+  null: "gray",
+};
+
+const winningCombinations = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+/*----- state variables -----*/
+
+/*----- cached elements  -----*/
+
+/*----- event listeners (Logic) -----*/
+
+/*----- render functions (No logic) -----*/
+function render() {}
+
+/*----- functions -----*/
+function main() {
+  render();
+}
+
+main();
